0815d413715214d371b3ec52e3d06bd3976d94b6
heitors1337 <heitorguilhermerqs@gmail.com>
2025-12-02 19:18:11 -0300

    Update banco.sql

--- src/main/banco.sql
+++ src/main/banco.sql
@@ -1,9 +1,29 @@
 CREATE DATABASE maesdb;
 USE maesdb;
+
 CREATE TABLE mae (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     telefone VARCHAR(20),
     endereco VARCHAR(200),
     data_aniversario DATE
+);
+
+CREATE TABLE status (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
+);
+
+INSERT INTO status (nome) VALUES
+('FUTURO'),
+('REALIZADO'),
+('CANCELADO');
+
+CREATE TABLE encontro (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data DATE NOT NULL,
+    mae_id INT NOT NULL,
+    status_id INT NOT NULL,
+    FOREIGN KEY (mae_id) REFERENCES mae(id),
+    FOREIGN KEY (status_id) REFERENCES status(id)
 );

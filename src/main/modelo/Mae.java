2613f8015f084c4b3b605b13d922599187c7666d
heitors1337 <heitorguilhermerqs@gmail.com>
2025-12-02 09:57:43 -0300

    Update Mae.java

--- src/main/modelo/Mae.java
+++ src/main/modelo/Mae.java
@@ -3,6 +3,53 @@ public class Mae {
     public String nome;
     public String telefone;
     public String endereco;
-    public int dataAniversario;
+    public String dataAniversario;
 
+    public Mae(int id, String nome, String telefone, String endereco, String dataAniversario){
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.dataAniversario = dataAniversario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getDataAniversario() {
+        return dataAniversario;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setDataAniversario(String dataAniversario) {
+        this.dataAniversario = dataAniversario;
+    }
 }
